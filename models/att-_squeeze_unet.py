b503e45754e0acc00fb44a43c1fd65eab4f79450
rubythalib33 <rubyabdullah14@gmail.com>
2022-06-26 13:00:24 +0700

    update everythings

--- models/att-_squeeze_unet.py
+++ models/att-_squeeze_unet.py
@@ -79,8 +79,11 @@ class AttentionBlock(nn.Module):
         )
     
     def forward(self, g, x):
+        print("--g", g.shape, self.w_g)
         g1 = self.w_g(g)
+        print("--g1", g1.shape)
         x1 = self.w_x(x)
+        print("--x1", x1.shape)
 
         psi = self.relu(g1+x1)
         psi = self.psi(psi)
@@ -93,14 +96,18 @@ class UpsamplingBlock(nn.Module):
     def __init__(self,input_channels, filters, squeeze, expand, strides, deconv_ksize, att_filters):
         super(UpsamplingBlock, self).__init__()
         self.upconv = nn.ConvTranspose2d(input_channels, filters, kernel_size=deconv_ksize, stride=strides)
-        self.fire = FireModule(filters, squeeze, expand)
-        self.attention = AttentionBlock(filters, att_filters)
+        self.fire = FireModule(filters+att_filters, squeeze, expand)
+        self.attention = AttentionBlock(input_channels, att_filters)
     
     def forward(self, x, g):
         d = self.upconv(x)
+        print("-d", d.shape)
         x = self.attention(d, g)
+        print("-x", x.shape)
         d = torch.concat([x, d], axis=1)
+        print("-d", d.shape)
         x = self.fire(d)
+        print("-x", x.shape)
 
         return x
 
@@ -111,33 +118,95 @@ class AttSqueezeUNet(nn.Module):
         self.channel_axis = 1
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels, 64, 3, stride=2, padding=3),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3,3), stride=2)
+            nn.ReLU(inplace=True)
         )
 
+        self.maxpooling_1 = nn.MaxPool2d(kernel_size=(3,3), stride=2)
+
+        self.dropout = nn.Dropout(0.2) if self.__dropout else None
+
         self.fire1 = FireModule(64, 16, 64)
-        self.fire2 = FireModule(64, 16, 64)
+        self.fire2 = FireModule(128, 16, 64)
         self.maxpooling_2 = nn.MaxPool2d(kernel_size=(3,3), stride=(2,2))
 
-        self.fire3 = FireModule(64,32,128)
-        self.fire4 = FireModule(128,32,128)
+        self.fire3 = FireModule(128,32,128)
+        self.fire4 = FireModule(256,32,128)
         self.maxpooling_3 = nn.MaxPool2d(kernel_size=(3,3), stride=(2,2))
 
-        self.fire5 = FireModule(128, 48, 192)
-        self.fire6 = FireModule(192, 48, 192)
-        self.fire7 = FireModule(192, 48, 192)
-        self.fire8 = FireModule(192, 48, 192)
+        self.fire5 = FireModule(256, 48, 192)
+        self.fire6 = FireModule(384, 48, 192)
+        self.fire7 = FireModule(384, 48, 256)
+        self.fire8 = FireModule(512, 48, 256)
 
-        self.upsampling1 = UpsamplingBlock(192, 192, squeeze=48, expand=192, strides=(1,1), deconv_ksize=(3), att_filters=96)
-        self.upsampling2
-        self.upsampling3
-        self.upsampling4
-        self.upsampling5
+        self.upsampling1 = UpsamplingBlock(512, 192, squeeze=48, expand=192, strides=(1,1), deconv_ksize=(3), att_filters=96)
+        self.upsampling2 = UpsamplingBlock(192, 128, squeeze=32, expand=128, strides=(1,1), deconv_ksize=(3), att_filters=64)
+        self.upsampling3 = UpsamplingBlock(128, 64, squeeze=16, expand=64, strides=(1,1), deconv_ksize=(3), att_filters=16)
+        self.upsampling4 = UpsamplingBlock(64, 32, squeeze=16, expand=32, strides=(1,1), deconv_ksize=(3), att_filters=4)
+        self.upsampling5 = nn.Upsample(size=(2,2))
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.upsampling6 = nn.Upsample(size = (2,2))
+
+        self.conv3  = nn.Sequential(
+            nn.Conv2d(64, n_class, 1),
+            nn.LogSoftmax() if n_class > 1 else nn.Sigmoid()
+        )
+    
+    def forward(self, x):
+        x0 = self.conv1(x)
+        x1 = self.maxpooling_1(x0)
+        print("x1",x1.shape)
+
+        x2 = self.fire1(x1)
+        x2 = self.fire2(x2)
+        x2 = self.maxpooling_2(x2)
+        print("x2",x2.shape)
+
+        x3 = self.fire3(x2)
+        x3 = self.fire4(x3)
+        x3 = self.maxpooling_3(x3)
+        print("x3", x3.shape)
+
+        x4 = self.fire5(x3)
+        x4 = self.fire6(x4)
+        print("x4",x4.shape)
+
+        x5 = self.fire7(x4)
+        x5 = self.fire8(x5)
+        print("x5",x5.shape)
+
+        if self.__dropout:
+            x5 = self.dropout(x5)
+        
+        d5 = self.upsampling1(x5,x4)
+        print("d5", d5.shape)
+        d4 = self.upsampling2(d5, x3)
+        print("d4", d4.shape)
+        d3 = self.upsampling3(d4, x2)
+        print("d3", d3.shape)
+        d2 = self.upsampling4(d3, x1)
+        print("d2", d2.shape)
+        d1 = self.upsampling5(d2)
+        print("d1", d1.shape)
+
+        d0 = torch.concat([d1,x0], dim=1)
+        d0 = self.conv2(d0)
+        d0 = self.upsampling6(d0)
+
+        d = self.conv3(d0)
+
+        return d
+
+
 
 
 
 if __name__ == "__main__":
-    model = AttFireModule(input_channel=3, squeeze=8, expand=16)
+    model = AttSqueezeUNet(in_channels=3, n_class=1)
     print(model)
     input_ = torch.randn([1,3,224,224], dtype=torch.float32)
     print(input_.shape)
